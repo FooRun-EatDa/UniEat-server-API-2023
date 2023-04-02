6955827d75353632eba399122389f12e4b8e540d
SooBong Kim <govmffpdltm@gmail.com>
2023-04-02 17:19:13 +0900

    개발 환경이 local과 배포환경의 차이점이 발생한 것에 대한 대응 처리

--- src/main/java/foorun/unieat/api/service/member/MemberSignInService.java
+++ src/main/java/foorun/unieat/api/service/member/MemberSignInService.java
@@ -3,10 +3,11 @@ package foorun.unieat.api.service.member;
 import foorun.unieat.api.model.domain.member.request.MemberSignIn;
 import foorun.unieat.api.model.domain.UniEatCommonResponse;
 import foorun.unieat.api.model.database.member.entity.UniEatMemberEntity;
-import foorun.unieat.api.model.database.member.UniEatMemberRepository;
+import foorun.unieat.api.model.database.member.repository.UniEatMemberRepository;
 import foorun.unieat.api.exception.UniEatForbiddenException;
 import foorun.unieat.api.service.UniEatCommonService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,9 +17,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class MemberSignInService extends UniEatCommonService<MemberSignIn> implements UserDetailsService {
+public class MemberSignInService implements UniEatCommonService<MemberSignIn>, UserDetailsService {
     private final UniEatMemberRepository memberRepository;
 
     @Override
--- src/main/java/foorun/unieat/api/service/member/MemberSignUpService.java
+++ src/main/java/foorun/unieat/api/service/member/MemberSignUpService.java
@@ -5,19 +5,21 @@ import foorun.unieat.api.exception.UniEatForbiddenException;
 import foorun.unieat.api.model.domain.member.request.MemberSignUp;
 import foorun.unieat.api.model.domain.UniEatCommonResponse;
 import foorun.unieat.api.model.database.member.entity.UniEatMemberEntity;
-import foorun.unieat.api.model.database.member.UniEatMemberRepository;
+import foorun.unieat.api.model.database.member.repository.UniEatMemberRepository;
 import foorun.unieat.api.service.UniEatCommonService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.Cacheable;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Cacheable
-public class MemberSignUpService extends UniEatCommonService<MemberSignUp> {
+public class MemberSignUpService implements UniEatCommonService<MemberSignUp> {
     private final UniEatMemberRepository memberRepository;
 
     @Override
