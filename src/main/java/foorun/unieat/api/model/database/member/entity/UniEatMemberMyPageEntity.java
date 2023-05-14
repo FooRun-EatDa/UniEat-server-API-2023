813918162a6b36308334a58cd312703a8f5b2b45
soobongkim <govmffpdltm@gmail.com>
2023-05-14 23:46:10 +0900

    mypage 한줄소개 컬럼 추가 및 update를 위한 함수 추가

--- src/main/java/foorun/unieat/api/model/database/member/entity/UniEatMemberMyPageEntity.java
+++ src/main/java/foorun/unieat/api/model/database/member/entity/UniEatMemberMyPageEntity.java
@@ -8,13 +8,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 
 /**
@@ -28,6 +31,7 @@ import javax.persistence.Table;
 @Getter
 @Builder
 @IdClass(UniEatMemberId.class)
+@DynamicUpdate
 public class UniEatMemberMyPageEntity extends UniEatBaseTimeEntity {
     /**
      * 회원 접속경로
@@ -46,12 +50,31 @@ public class UniEatMemberMyPageEntity extends UniEatBaseTimeEntity {
     /**
      * 회원 닉네임
      */
-    @Column(name="member_nickname", unique = true)
+    @Column(name = "member_nickname", unique = true, length = 50)
     private String nickname;
 
     /**
      * 회원 프로필사진
      */
-    @Column(name="member_profile_image")
+    @Column(name = "member_profile_image", length = 500)
     private String profileImagePath;
+
+    /**
+     * 자기소개 문구
+     */
+    @Column(name = "member_introduction", length = 50)
+    private String introduce;
+
+    public void changeNickname(@NotBlank String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void changeProfileImage(MultipartFile file) {
+        /* todo: image 파일 보관 후, 해당 경로 URL 가져오기 및 데이터 바꿔치기 구현 */
+        this.profileImagePath = file.getOriginalFilename();
+    }
+
+    public void changeIntroduction(String introduce) {
+        this.introduce = introduce;
+    }
 }
